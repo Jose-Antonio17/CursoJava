6bd8201a7c2e66e2c15f7c6d3be877f8159211d4
Tardes <Tardes@GET-09>
2024-12-13 19:24:49 +0100

    a

--- src/es/cursojava/arrays/ArraysBidimensionales.java
+++ src/es/cursojava/arrays/ArraysBidimensionales.java
@@ -1,5 +1,19 @@
 package es.cursojava.arrays;
 
 public class ArraysBidimensionales {
+	
+	public static void main(String[] args) {
+		int filas = 2;
+		int columnas = 6;
+		int [][] numeros = new int [filas][columnas];
+		
+		for (int fila=0; fila<numeros.length ; fila++) {
+			
+			for (int columna=0 ; columna < numeros[fila].length   ; columna++) {
+				System.out.println("["+fila+"]["+columna+"]: "+ numeros[fila][columna]);
+			}
+			
+		}
 
+	}
 }
--- src/es/cursojava/ejercicios/arrays/ArrayEjercicio5.java
+++ src/es/cursojava/ejercicios/arrays/ArrayEjercicio5.java
@@ -2,4 +2,6 @@ package es.cursojava.ejercicios.arrays;
 
 public class ArrayEjercicio5 {
 
+	
+	
 }
