b579052986003ebbf9a0060f22d84c77ef08f745
Jose-Antonio17 <ceronortizjose@gmail.com>
2025-02-11 20:45:33 +0100

    123

---
+++ src/es/cursojava/listas/VehiculosListas.java
@@ -0,0 +1,15 @@
+package es.cursojava.listas;
+
+import java.util.ArrayList;
+
+import es.cursojava.vehiculos.Vehiculo;
+
+public class VehiculosListas {
+
+	ArrayList<Vehiculo> lista = new ArrayList<>();
+	
+	lista.add(new Coche("Toyota", "Corolla", 2004, 180, "hibrido", 5, true));
+	list.add(new Camion());
+	list.add(new Motocicleta());
+
+}
