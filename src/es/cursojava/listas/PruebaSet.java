bf14399a9b8c83b752941a381dc65c8c38252b2e
Jose-Antonio17 <ceronortizjose@gmail.com>
2025-02-12 19:16:31 +0100

    mier

---
+++ src/es/cursojava/listas/PruebaSet.java
@@ -0,0 +1,24 @@
+package es.cursojava.listas;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import es.cursojava.poo.Alumno;
+
+public class PruebaSet {
+
+	public static void main(String[] args) {
+		
+		Set<Alumno> alumnos = new HashSet();
+		
+		alumnos.add(new Alumno("Jose", "Antonio", 8, 5, "jose@gmail.com"));
+		alumnos.add(new Alumno("Juan", "Garavito", 8, 5, "juan@gmail.com"));
+		
+		Iterator<Alumno> ite = alumnos.iterator();
+		System.out.println(ite.next().getNombre());
+		System.out.println(ite.next().getNombre());
+		//System.out.println(ite.next().getNombre());
+	}
+
+}
--- src/es/cursojava/listas/VehiculosListas.java
+++ src/es/cursojava/listas/VehiculosListas.java
@@ -1,15 +1,37 @@
 package es.cursojava.listas;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+import es.cursojava.vehiculos.Camion;
+import es.cursojava.vehiculos.Coche;
+import es.cursojava.vehiculos.Motocicleta;
 import es.cursojava.vehiculos.Vehiculo;
 
 public class VehiculosListas {
 
-	ArrayList<Vehiculo> lista = new ArrayList<>();
+	public static void main(String [] args) {
+		
+		Vehiculo coche1 = new Coche("Toyota", "Corolla", 2004, 180, "hibrido", 5, true);
+		Vehiculo motocicleta1 = new Motocicleta("Honda", "Monkey", 2024, 90, "diesel", true, 125);
+		Vehiculo camion1 = new Camion("Volvo", "FH", 2013, 100, "gasolina", 750, 4);
+		
+		ArrayList<Vehiculo> lista = new ArrayList<>();
+		
+		lista.add(coche1);
+		lista.add(motocicleta1);
+		lista.add(camion1);
+		
+		System.out.println(lista.size());
+		
+		for (int i= 0; i<lista.size();i++) {
+			lista.get(i).consultar();
+			System.out.println(lista.get(i));
+		}
+		
+		List<Vehiculo> lista2 = Arrays.asList(coche1,camion1,motocicleta1); 
+	}
 	
-	lista.add(new Coche("Toyota", "Corolla", 2004, 180, "hibrido", 5, true));
-	list.add(new Camion());
-	list.add(new Motocicleta());
-
+	
 }
