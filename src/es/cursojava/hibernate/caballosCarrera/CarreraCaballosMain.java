ddd03f1dbb8e876770c81950d2b4213da6c6ac4a
ceronortizjose@gmail.com <samuel2200@hotmail.es>
2025-04-10 20:08:30 +0200

    10-04

--- src/es/cursojava/hibernate/caballosCarrera/CarreraCaballosMain.java
+++
@@ -1,8 +0,0 @@
-package es.cursojava.hibernate.caballosCarrera;
-
-public class CarreraCaballosMain {
-    public static void main(String[] args) {
-        CarreraCaballosFront front = new CarreraCaballosFront("Carrera 1");
-        front.iniciaMenuCarrera();
-    }
-}
